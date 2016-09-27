65483c46d152d4721b827ee5e9a310de9b46837e
Eric Mascot <slek120@gmail.com>
2016-09-27 18:50:59 -0500

    remove surf and add title

--- plot_trajectory.m
+++ plot_trajectory.m
@@ -1,11 +1,13 @@
+% Import data
 t = importdata('t.txt');
 x = importdata('x.txt');
 y = importdata('y.txt');
 
+% Plot
 ax = gca;
 plot3(ax,x,y,t);
 ax.View = [0 90];
-ax.Title.String  = 'Predator Position'
+ax.Title.String  = 'Predator Position';
 ax.XLabel.String = 'x (v_{prey}s)';
 ax.YLabel.String = 'y (v_{prey}s)';
 ax.ZLabel.String = 't (s)';
