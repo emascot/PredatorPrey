46c8dbe5c4359b4432b3456016e8ab9a9a2215e7
Eric Mascot <slek120@gmail.com>
2016-09-28 11:47:24 -0500

    add color to trajectory plots

--- plot_trajectory.m
+++ plot_trajectory.m
@@ -2,12 +2,20 @@
 t = importdata('t.txt');
 x = importdata('x.txt');
 y = importdata('y.txt');
+N = size(x,2);
+RGB = hsv(N);
 
 % Plot
 ax = gca;
-plot3(ax,x,y,t);
+hold(ax, 'on')
+for i=1:1000
+    plot3(ax,x(:,i),y(:,i),t,'Color',RGB(i,:));
+end
+hold(ax, 'off')
+
 ax.View = [0 90];
 ax.Title.String  = 'Predator Position';
 ax.XLabel.String = 'x (v_{prey}s)';
 ax.YLabel.String = 'y (v_{prey}s)';
 ax.ZLabel.String = 't (s)';
+ax.YLim = [0.01 10];
--- plot_trajectory_movie.m
+++ plot_trajectory_movie.m
@@ -3,6 +3,8 @@ filename = 'trajectories.mp4';
 t = importdata('t.txt');
 x = importdata('x.txt');
 y = importdata('y.txt');
+N = size(x,2);
+RGB = hsv(N);
 
 % Initialize figure
 fig = figure;
@@ -10,7 +12,11 @@ ax = gca;
 fig.Position = [0 0 1440 800];
 
 % Plot
-plot3(ax,x,y,t);
+hold(ax, 'on')
+for i=1:N
+    plot3(ax,x(:,i),y(:,i),t,'Color',RGB(i,:));
+end
+hold(ax, 'off')
 ax.View = [0 90];
 ax.Title.String  = 'Predator Position';
 ax.XLabel.String = 'x (v_{prey}s)';
